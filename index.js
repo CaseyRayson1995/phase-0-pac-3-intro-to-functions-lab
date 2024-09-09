67c3e6ed41ba387338b5d9fe784d3e7a67d86c05
CaseyRayson1995 <caseyrayson1995@gmail.com>
2024-09-09 17:00:48 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,49 @@
+//Declearing the function shout
+function shout(string) {
+  }
+
+  "Hello!".toUpperCase(); // 'HELLO!'
+  //
+  function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string){
+
+  }
+  "Hello".toLowerCase();
+
+  function whisper(string){
+    return string.toLowerCase();
+  }
+
+  function logShout(string) {
+    
+    let upperString = string.toUpperCase();
+  
+    
+    console.log(upperString);
+  }
+
+  function logWhisper(string) {
+    
+    let lowerString = string.toLowerCase();
+
+  
+    
+    console.log(lowerString);
+  }
+  
+  function sayHiToHeadphonedRoommate(string) {
+    if (string.toLowerCase() === string) {
+        return "I can't hear you!";
+    }
+    else if (string.toUpperCase() === string) {
+        return "YES INDEED!";
+    }
+    else if (string ==="Let's have dinner together!"){
+        return "I would love to!"
+    }
+
+
+}
+
